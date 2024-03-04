36afb81105a862746312f9f51af95b5b048baaef
Jonathan Groberg <jongroberg@gmail.com>
2024-03-03 22:32:18 -0500

    created string_to_data, get_decoded_length, encode_rle, count_runs, to_hex_string. Also created tests for each in text.py

--- rle_program.py
+++ rle_program.py
@@ -1,5 +1,90 @@
 from console_gfx import ConsoleGfx
 
+
+def to_hex_string(data: list[int]) -> str:
+    hex_digits = "0123456789abcdef"  # the index of each char can be used to convert from decimal to hex
+    result = ""
+
+    for digit in data:
+        result += hex_digits[int(digit)]  # 15 should result in the 15th char (F) in the list
+    return result
+
+
+def count_runs(flat_data: list[int]) -> int:
+    count = 0
+    running_count = 0
+    current = None
+
+    for element in flat_data:  # for each element, check if it is a new number
+        if running_count >= 15:
+            count += 1
+            running_count = 0
+        if element != current:
+            count += 1
+            running_count = 0
+            current = element
+        else:
+            running_count += 1
+
+    return count
+
+
+def encode_rle(flat_data: list[int]) -> list[int]:
+    running_count = 0
+    current = flat_data[0]  # starts as the first element
+    arr = []
+
+    def append():
+        arr.append(running_count)
+        arr.append(current)
+
+    for i in range(len(flat_data)):  # uses a range because we need the check if it's the last index
+        element = flat_data[i]  # grab the current elem
+
+        if running_count >= 15:
+            append()
+            running_count = 0
+        if element != current:  # if a new run starts or last element is hit add to arr
+            append()
+            running_count = 1  # reset the count
+            current = element
+        else:
+            running_count += 1
+
+        if i == len(flat_data) - 1:
+            append()
+
+    return arr
+
+
+def get_decoded_length(rle_data: list[int]) -> int:  # sums every other element starting at 0,2,4,6...
+    total = 0
+    for i in range(0, len(rle_data), 2):
+        total += rle_data[i]
+    return total
+
+
+def decode_rle(rle_data: list[int]) -> list[int]:
+    arr = []
+    for i in range(0, len(rle_data), 2):  # goes over every 2 elements
+        count = rle_data[i]
+        elem = rle_data[i + 1]
+
+        for j in range(count):
+            arr.append(elem)
+    return arr
+
+
+def string_to_data(data_string: str) -> list[int]:
+    hex_digits = "0123456789abcdef"  # the index of each char can be used to convert from decimal to hex
+    arr = []
+
+    for digit in data_string:
+        arr.append(hex_digits.index(digit))
+
+    return arr
+
+
 if __name__ == '__main__':
     # Welcome Message
     # Display spectrum image message
@@ -25,7 +110,7 @@ if __name__ == '__main__':
         elif selected_option == 1:
             file_name = input("Enter name of file to load: ")
             image_data = ConsoleGfx.load_file(file_name)
-            print("Loaded file: " + file_name+"\n")
+            print("Loaded file: " + file_name + "\n")
         elif selected_option == 2:
             image_data = ConsoleGfx.test_image
         elif selected_option == 6:
---
+++ test.py
@@ -0,0 +1,13 @@
+from rle_program import *
+
+print(to_hex_string([3, 15, 6, 4]) == "3f64")
+print(count_runs(
+    [4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8,
+     7]))
+print(encode_rle([15, 15, 15, 4, 4, 4, 4, 4, 4]))
+print(encode_rle(
+    [4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8,
+     7]))
+print(get_decoded_length([3, 15, 6, 4]))
+print(string_to_data("3f64"))
+print(decode_rle([3,15,6,4]))
